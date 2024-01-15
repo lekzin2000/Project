166e867d912334ef80a2fa01eb5167dfce1c3c93
lekzin2000 <lekzin2000@gmail.com>
2024-01-15 10:45:59 +0100

    my first project commit

---
+++ Fiverr-LandingPage/script.js
@@ -0,0 +1,61 @@
+const initSlider = () => {
+    const imageList = document.querySelector(".slide-list .slides-wrapper");
+    const slideButtons = document.querySelectorAll(".slide-list .slide-button");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+    
+    //slide image according to slide button click
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({ left: scrollAmount, behaviour: "smooth" });
+        });
+    });
+
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "block";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
+    }
+
+    imageList.addEventListener("scroll", () => {
+        handleSlideButtons();
+    });
+}
+
+window.addEventListener("load", initSlider);
+
+// testimonial
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// inspiring
+
+
+
